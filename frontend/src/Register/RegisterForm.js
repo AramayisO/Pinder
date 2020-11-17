1df7415923ebe72b6d6caf5c5b742e643b059347
Steven Duran <stevenduran@gmail.com>
2020-11-16 17:50:09 -0800

    Basic form, no logic implemented.

--- frontend/src/Register/RegisterForm.js
+++ frontend/src/Register/RegisterForm.js
@@ -8,7 +8,68 @@ class RegisterForm extends React.Component {
             email: '',
             password: '',
             passwordConfirm: '',
+            name: '',
+            zip: '',
         }
+    }
+
+    render() {
+        const {buttonText, textColor, accentColor} = this.props;
+
+        const formStyle = {
+            color: textColor,
+        };
+
+        const buttonStyle = {
+            backgroundColor: accentColor,
+            borderColor: accentColor,
+            color: textColor,
+        };
+
+        return (
+            <div className="container">
+                <form style={formStyle} onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            value={this.email}
+                            onChange={this.handleEmailChange}
+                            placeholder="Email"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            value={this.password}
+                            onChange={this.handlePasswordChange}
+                            placeholder="Password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="passwordConfirm">Confirm Password</label>
+                        <input
+                            type="password"
+                            id="passwordConfirm"
+                            value={this.password}
+                            onChange={this.handlePasswordConfirmChange}
+                            placeholder="Confirm password"
+                        />
+                    </div>
+                    <div className="text-center">
+                        <button
+                            type="submit"
+                            className="btn btn-primary px-4"
+                            style={buttonStyle}>
+                                {buttonText}
+                        </button>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
