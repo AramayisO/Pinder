50fc1372d975cd161807d543c1e31719dcbf1c1d
Steven Duran <stevenduran@gmail.com>
2020-11-24 19:20:58 -0800

    Basic layout for register page. Authentication doesn't work.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import { AuthRoute } from './common';
 import LoginPage from './Login/LoginPage';
+import RegisterPage from './Register/RegisterPage'
 
 const Home = () => (<h1>Welcome to Pinder</h1>);
 
@@ -11,7 +12,8 @@ function App() {
     return (
         <Switch>
             <AuthRoute path='/' exact={true} component={Home} />
-            <Route path='/login' component={LoginPage} /> 
+            <Route path='/login' component={LoginPage} />
+            <Route path='/register' component={RegisterPage} />
         </Switch>
     );
 }
--- frontend/src/Register/RegisterForm.js
+++ frontend/src/Register/RegisterForm.js
@@ -1,116 +1,105 @@
-import React from 'react';
+import { useState } from 'react';
 
-class RegisterForm extends React.Component {
-    constructor(props) {
-        super(props);
+const RegisterForm = (props) => {
 
-        this.state = {
-            email: '',
-            password: '',
-            passwordConfirm: '',
-            name: '',
-            zip: '',
-            city: '',
-            street: '',
-            usState: '',
-        }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const [street, setStreet] = useState('');
+    const [city, setCity] = useState('');
+    const [usState, setUsState] = useState('');
+    const [zip, setZip] = useState('');
+
+    const handleSubmit = (event) => {
+        event.prevetDefault();
+        props.onSubmit(email, password, passwordConfirm);
     }
 
-    render() {
-        const {buttonText, textColor, accentColor} = this.props;
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={e=> setEmail(e.target.value)}
+                        placeholder="Email"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={e=> setPassword(e.target.value)}
+                        placeholder="Password"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="passwordConfirm">Confirm Password</label>
+                    <input
+                        type="password"
+                        id="passwordConfirm"
+                        value={passwordConfirm}
+                        onChange = {e=> setPasswordConfirm(e.target.value)}
+                        placeholder="Confirm password"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="street">Street address</label>
+                    <input
+                        type="text"
+                        id="street"
+                        value={street}
+                        onChange = {e => setStreet(e.target.value)}
+                        placeholder="Street"
+                    />
+                    <label htmlFor="city">City</label>
+                    <input
+                        type="text"
+                        id="city"
+                        value={city}
+                        onChange = {e => setCity(e.target.value)}
+                        placeholder="City"
+                    />
 
-        const formStyle = {
-            color: textColor,
-        };
-
-        const buttonStyle = {
-            backgroundColor: accentColor,
-            borderColor: accentColor,
-            color: textColor,
-        };
-
-        return (
-            <div className="container">
-                <form style={formStyle} onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={this.email}
-                            onChange={this.handleEmailChange}
-                            placeholder="Email"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={this.password}
-                            onChange={this.handlePasswordChange}
-                            placeholder="Password"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="passwordConfirm">Confirm Password</label>
-                        <input
-                            type="password"
-                            id="passwordConfirm"
-                            value={this.password}
-                            onChange={this.handlePasswordConfirmChange}
-                            placeholder="Confirm password"
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="street">Street address</label>
-                        <input
-                            type="text"
-                            id="street"
-                            value={this.street}
-                            onChange={this.handleStreetChange}
-                            placeholder="Street"
-                        />
-                        <label htmlFor="city">City</label>
-                        <input
-                            type="text"
-                            id="city"
-                            value={this.city}
-                            onChange={this.handleCityChange}
-                            placeholder="City"
-                        />
-
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="usState">State</label>
-                        <input 
-                            type="text"
-                            id="usState"
-                            value={this.usState}
-                            onChange={this.handleStateChange}
-                            placeholder="State"
-                        />
-                        <lable htmlFor="zip">Zip</lable>
-                        <input
-                            type="text"
-                            id="zip"
-                            value={this.zip}
-                            onChange={this.handleZipChange}
-                            placeholder="zip"
-                        />
-                    </div>
-                    <div className="text-center">
-                        <button
-                            type="submit"
-                            className="btn btn-primary px-4"
-                            style={buttonStyle}>
-                                {buttonText}
-                        </button>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+                </div>
+                <div className="form-group">
+                    <label htmlFor="usState">State</label>
+                    <input 
+                        type="text"
+                        id="usState"
+                        value={usState}
+                        onChange={e => setUsState(e.target.value)}
+                        placeholder="State"
+                    />
+                    <lable htmlFor="zip">Zip</lable>
+                    <input
+                        type="text"
+                        id="zip"
+                        value={zip}
+                        onChange={e => setZip(e.target.value)}
+                        placeholder="zip"
+                    />
+                </div>
+                {props.error &&
+                <div className="alert alert-danger" role="alert">
+                    {props.error}
+                </div>
+                }
+                <div className="text-center">
+                    <button
+                        type="submit"
+                        className="btn btn-primary px-4">
+                            Register
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default RegisterForm;
